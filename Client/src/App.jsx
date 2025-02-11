7c940248391449dfaa345bdd51e2e2ae4979e788
vishal <vishalaggarwal4469359@gmail.com>
2025-02-11 11:49:22 +0530

    changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import Navbar from "./Components/NavBar";
-import {Button} from "./Components/ui/button"
 import React from 'react';
 import './index.css'
 import { DropzoneButton } from './Components/Dropzone/DropzoneButton';
@@ -9,8 +8,6 @@ function App() {
   return (
     
     <>
-
-      <Button>Hello</Button>
       <Navbar />
       <DropzoneButton/>
       </>
